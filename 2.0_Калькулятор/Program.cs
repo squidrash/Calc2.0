2d5c1641afcab617dbbca7e32e138fc45eb031a6
nikitarasevskij <squidrash777@gmail.com>
2020-02-27 08:44:19 +0300

    switch разбил на методы

--- 2.0_Калькулятор/Program.cs
+++ 2.0_Калькулятор/Program.cs
@@ -9,7 +9,7 @@ namespace _2._0_Калькулятор
             while (true)
             {
                 Console.Clear();
-                double x, y, n;
+                double x, y, z;
                 int op;
                 //комментарий
 
@@ -28,63 +28,10 @@ namespace _2._0_Калькулятор
                         op = int.Parse(Console.ReadLine());
                         if (op == 5 || op == 6 || op == 7 || op == 8
                             || op == 9 || op == 10 || op == 11 || op == 12 || op == 13)
-                        {
-                            switch (op)
-                            {
-                                case 5:
-                                    n = Sin(x);
-                                    Console.WriteLine(n);
-                                    break;
-                                case 6:
-                                    n = Cos(x);
-                                    Console.WriteLine(n);
-                                    break;
-                                case 7:
-                                    n = Tan(x);
-                                    Console.WriteLine(n);
-                                    break;
-                                case 8:
-                                    n = ATan(x);
-                                    Console.WriteLine(n);
-                                    break;
-                                case 9:
-                                    n = Ln(x);
-                                    Console.WriteLine(n);
-                                    //Console.WriteLine($"Натуральный логорифм числа{x} = {Math.Log(x)}");
-                                    break;
-                                case 10:
-                                    n = Log10(x);
-                                    Console.WriteLine(n);
-                                    //Console.WriteLine($"Десятичный логорифм от {x} = {Math.Log10(x)}");
-                                    break;
-                                case 11:
-                                    n = Exp(x);
-                                    Console.WriteLine(n);
-                                    //Console.WriteLine(Math.Exp(x));s
-                                    break;
-                                case 12:
-                                    try
-                                    {
-                                        if (x % 1 == 0 && x >= 0)
-                                        {
-                                            int i = (int)x;
-                                            n = Factorial(i);
-                                            Console.WriteLine(n);
-                                        }
-                                        else
-                                            throw new Exception("Число должно быть целое и больше 0");
-                                    }
-                                    catch (Exception e)
-                                    {
-                                        Console.WriteLine(e.Message);
-                                    }
-                                    break;
-                                case 13:
-                                    n = OneX(x);
-                                    Console.WriteLine(n);
-                                    break;
+                        {                            
+                            z = Calculate1(x, op);
+                            Console.WriteLine(z);
 
-                            }
                         }
 
 
@@ -94,51 +41,23 @@ namespace _2._0_Калькулятор
                             {
                                 Console.WriteLine("Введите второе число");
                                 y = double.Parse(Console.ReadLine());
-                                switch (op)
-                                {
-                                    case 1:
-                                        n = Sum(x, y);
-                                        Console.WriteLine(n);
-                                        break;
-                                    case 2:
-                                        n = Subtract(x, y);
-                                        Console.WriteLine(n);
-                                        break;
-                                    case 3:
-                                        n = Multiply(x, y);
-                                        Console.WriteLine(n);
-                                        break;
-                                    case 4:
-                                        if (y == 0)
-                                            throw new Exception("Деление на 0");
-                                        else
-                                        {
-                                            n = Divide(x, y);
-                                            Console.WriteLine(n);
-                                        }
-                                        break;
-                                    case 14:
-                                        n = Pow(x, y);
-                                        Console.WriteLine(n);
-                                        break;
-                                    default:
-                                        break;
-
-                                }
+                                z = Calculate2(x, y, op);
+                                Console.WriteLine(z);
                             }
                             catch (Exception)
                             {
-                                Console.WriteLine("Неверный формат. Введите второе число ");
+                                Console.WriteLine("Неверный формат числа (2)");
                             }
+                            
                         }
-                        //else
-                        //{
-                        //    throw new Exception("Неверная команда");
-                        //}
+                        else
+                        {
+                            throw new Exception("Неверная команда");
+                        }
                     }
                     catch (Exception)
                     {
-                        Console.WriteLine("Неизвестная команда");
+                        Console.WriteLine("Неверный формат команды");
                     }
 
                 }
@@ -152,49 +71,147 @@ namespace _2._0_Калькулятор
 
                 Console.ReadLine();
 
+                static double Calculate1(double x, int op)
+                {
+                    double n;
+                    try
+                    {
+                        switch (op)
+                        {
+                            case 5:
+                                n = Sin(x);
+                                return n;
+                            case 6:
+                                n = Cos(x);
+                                return n;
+
+                            case 7:
+                                n = Tan(x);
+                                return n;
+
+                            case 8:
+                                n = ATan(x);
+                                return n;
+
+                            case 9:
+                                n = Ln(x);
+                                return n;
+
+                            case 10:
+                                n = Log10(x);
+                                return n;
+
+                            case 11:
+                                n = Exp(x);
+                                return n;
+
+
+                            case 12:
+                                try
+                                {
+                                    if (x % 1 == 0 && x >= 0)
+                                    {
+                                        int i = (int)x;
+                                        n = Factorial(i);
+                                        return n;
+                                    }
+                                    else
+                                        throw new Exception("Число должно быть целое и больше 0");
+                                }
+                                catch (Exception)
+                                {
+                                    throw new Exception("ошибка в факториале");
+                                }
+                            case 13:
+                                n = OneX(x);
+                                return n;
+
+                        }
+                    }
+                    catch(Exception)
+                    {
+                        throw new Exception("ошибка в catch calculate1");
+                    }
+                    return n = 123456789;
+                    
+                }
+                static double Calculate2(double x, double y, int op)
+                {
+                    double n;
+                    try
+                    {
+                        switch (op)
+                        {
+                            case 1:
+                                n = Sum(x, y);
+                                return n;
+                                
+                            case 2:
+                                n = Subtract(x, y);
+                                return n;
+                            case 3:
+                                n = Multiply(x, y);
+                                return n;
+                            case 4:
+                                if (y == 0)
+                                    throw new Exception("Деление на 0");
+                                else
+                                {
+                                    n = Divide(x, y);
+                                    return n;
+                                }                                
+                            case 14:
+                                n = Pow(x, y);
+                                return n;
+                            default:
+                                break;
+
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw new Exception("ошибка в catch calculate2");
+                    }
+                    return n= 123456789;
+                }
+
                 static double Sin(double degrees)
                 {
                     double angle = Math.PI * degrees / 180;
                     double sinAngle = Math.Sin(angle);
-                    return sinAngle;
-                    //Console.WriteLine($"Синус угла {degrees} = {sinAngle}");
+                    return sinAngle;                    
                 }
                 static double Cos(double degrees)
                 {
                     double angle = Math.PI * degrees / 180;
                     double cosAngle = Math.Cos(angle);
-                    return cosAngle;
-                    //Console.WriteLine($"Косинус угла {degrees} = {cosAngle}");
+                    return cosAngle;                    
                 }
                 static double Tan(double degrees)
                 {
                     double angle = Math.PI * degrees / 180;
                     double tanAngle = Math.Tan(angle);
-                    return tanAngle;
-                    //Console.WriteLine($"Тангенс угла {degrees} = {tanAngle}");
+                    return tanAngle;                    
 
                 }
                 static double ATan(double degrees)
                 {
                     double angle = Math.PI * degrees / 180;
                     double atanAngle = Math.Atan(angle);
-                    return atanAngle;
-                    //Console.WriteLine($"Арктангенс угла {degrees} = {atanAngle} ");
+                    return atanAngle;                    
                 }
                 static int Factorial(int x)
                 {
                     if (x == 0)
-                        return 1;
-                    //Console.WriteLine("1s");
+                        return 1;                    
                     else
                     {
                         int result = 1;
                         for (int i = 1; i <= x; i++)
                         {
                             result *= i;
-                        }
-                        return result;
-                        //Console.WriteLine($"Факториал числа {x} = {result}");
+                        }                        
+                        return result;                        
                     }
                 }
 
