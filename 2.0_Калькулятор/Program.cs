d8da0000f415c29b1d7c64b3bc9163df19c56d74
nikitarasevskij <squidrash777@gmail.com>
2020-02-27 07:37:26 +0300

    init

---
+++ 2.0_Калькулятор/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._0_Калькулятор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
